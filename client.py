d55e385a4d7384322218ce1a5482d244ac61897d
Shahar Moshonov <shaharmosho@gmail.com>
2022-11-06 17:37:35 +0200

    add person

--- client.py
+++ client.py
@@ -5,17 +5,12 @@ args = sys.argv
 ip = args[1]
 port = (int)(args[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-count = 3 ## Amount of messages
+s.sendto(b'', (ip, port))
 
 while True:
-    if (len(args) > count):
-        command = args[count] + ' ' + args[count + 1]
-        commandBytes = str.encode(command)
-        
-        count = count + 2
-        s.sendto(commandBytes, (ip, port))
-        data, addr = s.recvfrom(1024)
-        print(str(data), addr)
-
-
+    args = sys.argv
+    message = input()
+    s.sendto(str.encode(message), (ip, port))
+    data, addr = s.recvfrom(1024)
+    print(data.decode("utf-8"))
 ##s.close()
--- server.py
+++ server.py
@@ -2,9 +2,33 @@ import socket
 import sys
 
 def addToGroup(address, name):
-  ##justname = name.split('b')[1]
+  
   if name not in dict:
-    dict[address] = (str)(name).split()[1].split("'")[0]
+    ## Add the member to the group
+    myName = (str)(name).split()[1].split("'")[0]
+    dict[address] = myName
+
+    ## Send to the client all the members in the group
+
+    firstName = (str)(list(dict.values())[0])
+
+
+    if (firstName != myName):
+        groupNames = firstName 
+        for i in range(1, len(dict)):
+          value = (str)(list(dict.values())[i])
+          if (value != myName):
+            groupNames += ", " + (str)(value)
+            
+        s.sendto(str.encode(groupNames), address)
+
+    ## Send a message to all the members that that someone has joined
+    for i in range(0, len(dict)):
+        value = (str)(list(dict.values())[i])
+        if (value != myName):
+          joinMessage = myName + ' has joined'
+          s.sendto(str.encode(joinMessage), list(dict.keys())[i])
+      
 
 
 
@@ -18,15 +42,13 @@ dict = {}
 
 while True:
     data, addr = s.recvfrom(1024)
-    num = (str)(data.split()[0]).split("'")[1]
-    if (num == '1'):
-      addToGroup(addr, str(data))
-
-    
-    print(str(data), addr)
-    print(num)
-    print(dict)
-    s.sendto(data.upper(), addr)
+    print(data)
+    if (data.decode("utf-8") != "b''"):
+      num = (str)(data.split()[0]).split("'")[1]
+      if (num == '1'):
+        addToGroup(addr, str(data))
+      
+      print(dict)
 
 
   
